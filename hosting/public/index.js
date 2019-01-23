8927ccb5e10465246f9a11cdcca97a99e1016dcc
Yuji Sasaki <jyu0414@gmail.com>
2019-01-23 12:33:49 +0900

    削除機能の実装

--- hosting/public/index.js
+++ hosting/public/index.js
@@ -15,11 +15,19 @@ firebase.auth().onAuthStateChanged(function(user) {
             $("#greeting").text("Hi, " + snapshot.child("firstName").val());
             
         });
-
         ref.child("projects").on('value', function(snapshot) {
-          snapshot.forEach(function(child){
-            makeCard(child.key);
-          });
+          $("#cardContainer").empty();
+          if(snapshot.numChildren() == 0)
+          {
+            $("#noprojectCard").collapse("show");
+          } 
+          else {
+            snapshot.forEach(function(child){
+              makeCard(child.key);
+            });
+          }
+          $("#loading").remove();
+          
         });
       
     } else {
@@ -46,7 +54,7 @@ firebase.auth().onAuthStateChanged(function(user) {
         image = firebase.storage().ref(firebase.auth().currentUser.uid + "/" + snapshot.child("latestImage").val());
       }
       else{
-        image = firebase.storage().ref("share/44693028.jpeg");
+        image = firebase.storage().ref("share/kasgaiTemplateImage.png");
       }
       image.getDownloadURL().then(function(url) {
       $("#"+id + "image").attr("src",url);
@@ -55,7 +63,9 @@ firebase.auth().onAuthStateChanged(function(user) {
       });
 
       $("#cardContainer").append(
-        "<div class='col-sm-6'><div class='card mb-3'><img id='"+ id + "image" +"' class='card-img-top' alt='...'><div class='card-body'><h5 class='card-title'>" + title + "</h5>"
+        "<div class='col-sm-6'><div class='card mb-3' id='"+ id+"Card"+"'><img id='"+ id + "image" +"' class='card-img-top' alt='...'>"
+        +"<div class='card-img-overlay text-right'><button type='button' id='"+ id + "SettingButton" + "' class='btn btn-outline-info'>Settings</button></div>"
+        +"<div class='card-body'><h5 class='card-title'>" + title + "</h5>"
       +"<h6 class='card-subtitle mb-2 text-muted'>" + template + "</h6>"
       +"<p class='card-text'>" + description+ "</p>"
       +"</div><ul class='list-group list-group-flush'>"+
@@ -63,6 +73,20 @@ firebase.auth().onAuthStateChanged(function(user) {
                   "<li class='list-group-item'><a href="+ "yattoko/index.html?id=" + id +" class='card-link'>Open in Yattoko</a></li>"+
                   "<li class='list-group-item'><a href="+ "shikkui/index.html?id=" + id +" class='card-link'>Open in Shikkui</a></li>"+
                 "</ul></div></div>");
+
+        $("#"+id+"SettingButton").on( "click", function(event){
+            let id = $(event.target).attr("id").replace("SettingButton","");
+            $("#modalDeleteButton").attr("targetProjectId",id);
+            $("#modalTitle").text($("#"+id+"Card").find(".card-title").text() + " の設定");
+            $("#modalDescription").text($("#"+id+"Card").find(".card-text").text());
+            $("#settingModal").modal();
+        });
     });
     
   }
+
+  $("#modalDeleteButton").on("click",function(event){
+    let id = $(event.target).attr("targetProjectId");
+    firebase.database().ref("users/" + firebase.auth().currentUser.uid + "/projects/" + id).remove();
+    $("#settingModal").modal('hide');
+  })
